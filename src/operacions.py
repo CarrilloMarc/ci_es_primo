1965edd614cec57f97535d1b6ea3a1c27505e2a0
Carrillo Ramírez Marc <marcarram3@alu.edu.gva.es>
2025-02-03 09:37:41 +0100

    "Commit amb error"

--- src/operacions.py
+++ src/operacions.py
@@ -1,4 +1,4 @@
-def suma_enteros(sumando1, sumando2):
+def suma_enteros(sumando1, sumando2)
     if not isinstance(sumando1, int) or not isinstance(sumando2, int):
         raise TypeError('Proporciona solo enteros')
     else:
